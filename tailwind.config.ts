ac3bbcc0292e99762c156093ef8e06618312f275
CrisLavalley <crislavalley@gmail.com>
2025-04-11 01:10:16 -0600

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+
+import type { Config } from "tailwindcss";
+const config: Config = {
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: {} },
+  plugins: [require("@tailwindcss/typography")],
+};
+export default config;
